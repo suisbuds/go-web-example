168ea9b9d0efe28bc711f6f725b23473fd1e3619
suisbuds <suisbuds@gmail.com>
2024-12-17 15:39:08 +0800

    feat(pkg/convert): add convert.go

---
+++ pkg/convert/convert.go
@@ -0,0 +1,29 @@
+package convert
+
+import "strconv"
+
+type ConvertStr string
+
+func (s ConvertStr) String() string {
+	return string(s)
+}
+
+func (s ConvertStr) Int() (int, error) {
+	v, err := strconv.Atoi(s.String())
+	return v, err
+}
+
+func (s ConvertStr) MustInt() int {
+	v, _ := s.Int()
+	return v
+}
+
+func (s ConvertStr) UInt32() (uint32, error) {
+	v, err := strconv.Atoi(s.String())
+	return uint32(v), err
+}
+
+func (s ConvertStr) MustUInt32() uint32 {
+	v, _ := s.UInt32()
+	return v
+}
