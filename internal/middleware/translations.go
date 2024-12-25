824d58cc68474983f83217939ad280d09bab9a8d
suisbuds <suisbuds@gmail.com>
2024-12-25 16:57:53 +0800

    feat(translation.go): add translation func to parameters verification

---
+++ internal/middleware/translations.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/ja"
+	"github.com/go-playground/locales/zh"
+	"github.com/go-playground/universal-translator"
+	validator "github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	ja_translations "github.com/go-playground/validator/v10/translations/ja"
+)
+
+func Translations() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uni := ut.New(en.New(), zh.New(), ja.New())
+		locale := c.GetHeader("locale")
+		trans, _ := uni.GetTranslator(locale)
+		v, ok := binding.Validator.Engine().(*validator.Validate)
+		if ok {
+			switch locale {
+			case "zh":
+				_ = zh_translations.RegisterDefaultTranslations(v, trans)
+				break
+			case "en":
+				_ = en_translations.RegisterDefaultTranslations(v, trans)
+				break
+			case "ja":
+				_ = ja_translations.RegisterDefaultTranslations(v,trans)
+			default:
+				_ = en_translations.RegisterDefaultTranslations(v, trans)
+				break
+			}
+			c.Set("trans", trans)
+		}
+
+		c.Next()
+	}
+}
