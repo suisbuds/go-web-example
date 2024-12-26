a1e780965d01d6f7800f3b2b9da5a76cb4cfffa7
suisbuds <suisbuds@gmail.com>
2024-12-26 11:38:58 +0800

    feat(tag.go): encapsulate tag model functions

--- internal/models/tag.go
+++ internal/models/tag.go
@@ -1,6 +1,11 @@
 package models
 
-import "github.com/suisbuds/miao/pkg/app"
+import (
+	"github.com/suisbuds/miao/pkg/app"
+	"gorm.io/gorm"
+)
+
+// 封装 Tag 模块对应的数据操作
 
 type Tag struct {
 	*Model
@@ -8,11 +13,81 @@ type Tag struct {
 	State uint8  `json:"state"`
 }
 
+// 实现 Gorm 的接口，指定 Tag 对应的 pg 表名
 func (t Tag) TableName() string {
 	return "miao_tag"
 }
 
+// Swagger 文档生成
 type TagSwagger struct {
 	List  []*Tag
 	Pager *app.Pager
+}
+
+// 统计符合条件的 Tag 数量
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// 获取 Tag 列表
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	// where 筛选过滤
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+// 根据 ID 列表获取对应 Tag 列表
+func (t Tag) ListByIDs(db *gorm.DB, ids []uint32) ([]*Tag, error) {
+	var tags []*Tag
+	db = db.Where("state = ? AND is_del = ?", t.State, 0)
+	err := db.Where("id IN (?)", ids).Find(&tags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+// 根据 ID 获取 Tag
+func (t Tag) Get(db *gorm.DB) (Tag, error) {
+	var tag Tag
+	err := db.Where("id = ? AND is_del = ? AND state = ?", t.ID, 0, t.State).First(&tag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return tag, err
+	}
+
+	return tag, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB, values interface{}) error {
+	return db.Model(&t).Where("id = ? AND is_del = ?", t.ID, 0).Updates(values).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
 }
